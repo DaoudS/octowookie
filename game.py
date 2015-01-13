0c271befb9c331c3300523f70d2520e4325a7267
DaoudS <daoud.sogoba@gmail.com>
2015-01-13 09:08:30 -0500

    Added icon for player ship

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
 
-    pacman_image = pygame.image.load("sprites/pacman-small.png")
+    pacman_image = pygame.image.load("sprites/mfalcon.png")
     screen.blit(pacman_image, (10, 10))
 
     pygame.display.update()
