951b8e1ce29d2eb86c0e5252c8b71c5ca2e092df
DaoudS <daoud.sogoba@gmail.com>
2015-02-05 09:05:32 -0500

    Various additions and game changes
    	Added the main game loop to a large game class to better help with code segmentation.
    	The same has been done for the player.
    	Additoins of title and fps indication to the title for the game window.
    	Additions of various game sprites and skins, aswell as music and sound effects.

--- game.py
+++ game.py
@@ -1,24 +1,58 @@
-#!/usr/bin/python
-
+#!/ usr/bin/python
 import pygame
+from pygame.locals import *
 
-def main():
-    pygame.init()
-    #Colors
-    red   = 255,0,0
-    green = 0,255,0
-    blue  = 0,0,255
-    white = 255,255,255
+class Game(object):
+    def __init__(self):
+        pygame.init()
 
-    screen = pygame.display.set_mode((640, 480))
-    screen.fill(white)
+        self.window = pygame.display.set_mode((1024, 728)) 
 
-    player_image = pygame.image.load("sprites/mfalcon.png")
-    screen.blit(player_image, (300, 240))
-    pygame.display.update()
+        self.clock = pygame.time.Clock()
+        
+        pygame.key.set_repeat(50,50)
+        
+        pygame.display.set_caption('The Game %d fps' % self.clock.get_fps())
 
-    while True:
-        pass
+    def run(self):
+        running = True
+
+        while running:
+            self.clock.tick()
+            running = self.events()
+            pygame.display.update()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return False
+
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    return False
+        return True
+
+class Player:
+    def __init__(self):
+        self.image_load = pygame.image.load("res/graphics/mfalcon.png").convert_alpha()
+        self.x = 0
+        self.y = 0
+        self.angle = 0
+    def move(self, event):
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            self.y = self.y - 5
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            self.y = self.y + 5
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            self.x = self.x - 5
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            self.x = self.x + 5
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            pass
+    def draw(self, screen):
+        self.image = pygame.transform.rotate(self.image_load, self.angle)
+        self.screen.blit(image, (self.x, self.y))
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.run()
